7e2ae7dc9cea88d6bc11e855d0e3066c1b0a0a83
Bayu Sedana <bayusedana26@gmail.com>
2022-08-13 11:32:45 +0700

    update param

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/bayusedana26/golang-bayu
+module github.com/bayusedana26/golang-bayu/v2
 
 go 1.19
--- say-hello.go
+++ say-hello.go
@@ -1,5 +1,5 @@
 package golang_bayu
 
-func SayHello() string {
-	return "Hello World"
+func SayHello(name string) string {
+	return "Hello World" + name
 }
